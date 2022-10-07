234262428f77fbddc35712c66291a58b8c8ffb4b
Timkajke <105926655+Timkajke@users.noreply.github.com>
2022-10-07 21:25:29 +0700

    first lesson js

---
+++ access.js
@@ -0,0 +1,13 @@
+var isAdmin = confirm ('Are you admin?');
+if (isAdmin == true) {
+var password = prompt ('Enter password');
+password == 'iambatman' ? alert('Hello!') : alert ('Wrong!');
+// if (password == 'iambatman')
+// alert("Hello!");
+// else 
+// alert('Wrong!') 
+// else 
+}
+else {
+alert("User is incorrect") 
+}
